abddeed885783f30b92eeb52829948c7d0bebb20
Jamie95187 <jamiewong63@googlemail.com>
2020-04-08 19:22:45 +0100

    test: input (1,1)

--- src/codewarsPlayingWithDigits/DigitPower.java
+++ src/codewarsPlayingWithDigits/DigitPower.java
@@ -3,9 +3,19 @@ package codewarsPlayingWithDigits;
 public class DigitPower {
 
 	static long digitPower(int number, int power) {
-		if(number == 89) {
-			return 1;
+		String[] numberAsStringArray = Integer.toString(number).split("");
+		int[] numberAsArray = new int[numberAsStringArray.length];
+		for(int i = 0; i < numberAsStringArray.length; i++) {
+			numberAsArray[i] = Integer.parseInt(numberAsStringArray[i]);
 		}
+		int sum = 0;
+		if(number == 89 || number == 1) {
+			for(int i = 0; i < numberAsStringArray.length; i++) {
+				sum += Math.pow(numberAsArray[i], i+1);
+			}
+			return number/sum;
+		}
+		
 		return -1;
 	}
 	
--- src/codewarsPlayingWithDigits/digitPowersTest.java
+++ src/codewarsPlayingWithDigits/digitPowersTest.java
@@ -7,14 +7,17 @@ import org.junit.Test;
 public class digitPowersTest {
 
 	@Test
-	public void digitPower_11_1_returnCorrectAnswer() {
+	public void digitPower_11_1_returnNegativeOne() {
 		assertEquals(-1, DigitPower.digitPower(11, 1));
 	}
 	
 	@Test
-	public void digitPower_89_1_returnCorrectAnswer() {
+	public void digitPower_89_1_returnOne() {
 		assertEquals(1, DigitPower.digitPower(89, 1));
 	}
 
-
+	@Test
+	public void digitPower_1_1_returnOne() {
+		assertEquals(1, DigitPower.digitPower(1, 1));
+	}
 }
