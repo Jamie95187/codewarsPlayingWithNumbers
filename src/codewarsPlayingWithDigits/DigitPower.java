77f6170f3a23c484985cfefbdb2c46f59829ed71
Jamie95187 <jamiewong63@googlemail.com>
2020-04-08 18:54:55 +0100

    test: input = 89,1

--- src/codewarsPlayingWithDigits/DigitPower.java
+++ src/codewarsPlayingWithDigits/DigitPower.java
@@ -3,6 +3,9 @@ package codewarsPlayingWithDigits;
 public class DigitPower {
 
 	static long digitPower(int number, int power) {
+		if(number == 89) {
+			return 1;
+		}
 		return -1;
 	}
 	
--- src/codewarsPlayingWithDigits/digitPowersTest.java
+++ src/codewarsPlayingWithDigits/digitPowersTest.java
@@ -10,5 +10,11 @@ public class digitPowersTest {
 	public void digitPower_11_1_returnCorrectAnswer() {
 		assertEquals(-1, DigitPower.digitPower(11, 1));
 	}
+	
+	@Test
+	public void digitPower_89_1_returnCorrectAnswer() {
+		assertEquals(1, DigitPower.digitPower(89, 1));
+	}
+
 
 }
